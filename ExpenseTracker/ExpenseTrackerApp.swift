9fee96ee4a3026d619af00b558571f4e1708c884
ArtyomBeldeiko <artyom.beldeiko@gmail.com>
2022-07-10 00:06:17 +0300

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Artyom Beldeiko on 10.07.22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
